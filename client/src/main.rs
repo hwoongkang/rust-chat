419cb07ebc70f0ddf4c2c256ec91917e90a65bf3
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2022-12-12 16:37:24 +0900

    simple TCP Stream

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    // socket connect to 127.0.0.1:8000 all at once
+    let stream = std::net::TcpStream::connect("127.0.0.1:8000").unwrap();
+    println!("{:?}", stream);
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
+use std::{
+    io::Read,
+    net::{Ipv4Addr, SocketAddrV4},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let socket = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8000);
+    // listen
+    let listener = std::net::TcpListener::bind(socket).unwrap();
+    // accept
+
+    // read
+    loop {
+        match listener.accept() {
+            Ok((mut stream, _)) => {
+                let mut buf = [0; 1024];
+                match stream.read(&mut buf) {
+                    Ok(_) => {
+                        let s = std::str::from_utf8(&buf).unwrap();
+                        println!("we are here, {}", s);
+                    }
+                    Err(e) => println!("failed to read from socket; err = {:?}", e),
+                }
+            }
+            Err(e) => println!("failed to accept socket; err = {:?}", e),
+        }
+    }
 }
